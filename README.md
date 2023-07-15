d5eea915aed946fdf97f1537bfc47cc249f9e04a
Teoneag <teodor.neagoe.climber@gmail.com>
2023-07-15 20:20:58 +0300

    initial-commit-step-0

---
+++ README.md
@@ -0,0 +1,12 @@
+# Goal
+Personal assistant (like Jarvis form Iron Man)
+
+# Plan
+1. Make chat like interface (then add sound -> text, text -> sound, then also img...)
+2. Make it able to detect from props parts of data in ("learning") and data out ("giving responses")
+3. Handle data in
+4. Handle data out
+Later
+- add more languages
+
+# ToDo
---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      darkTheme: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.dark,
+      ),
+      themeMode: ThemeMode.system, // TODO: make it changable
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Jarvis'),
+      ),
+    );
+  }
+}
